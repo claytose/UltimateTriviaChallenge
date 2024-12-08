3aebd04de32dbd77f5865cc5ed67e7e09db70836
Sean Clayton <claytose@oregonstate.edu>
2024-12-08 13:50:16 -0800

    Fixed "Show Rules" modal.

--- public/script.js
+++ public/script.js
@@ -9,4 +9,23 @@ document.addEventListener('DOMContentLoaded', function() {
       document.querySelector('form').submit();
     }
   }, 1000);
+
+  // Modal handling
+  var modal = document.getElementById("rulesModal");
+  var btn = document.getElementById("rulesButton");
+  var span = document.getElementsByClassName("close")[0];
+
+  btn.onclick = function() {
+    modal.style.display = "block";
+  }
+
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
 });
